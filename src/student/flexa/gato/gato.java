cfb4ddd4ee87881d7854ab352b106796f469be71
oldflexa <89150977+oldflexa@users.noreply.github.com>
2021-09-09 19:42:49 -0300

    frist commit

---
+++ src/student/flexa/gato/gato.java
@@ -0,0 +1,61 @@
+package student.flexa.gato;
+
+import java.util.Objects;
+
+public class gato {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+	public gato() {}
+	
+	public gato(String nome, String cor, Integer idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cor, idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		gato other = (gato) obj;
+		return Objects.equals(cor, other.cor) && Objects.equals(idade, other.idade) && Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Gato [nome=" + nome + ", cor=" + cor + ", idade=" + idade + "]";
+	}
+	
+	
+	
+   /* public Gato (String nome; String cor; Integer idade;) {
+        this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+		
+}*/
+	
+}
---
+++ src/student/primeiroprograma.java
@@ -0,0 +1,23 @@
+package student;
+
+import student.primeiroprograma.gato;
+
+public class primeiroprograma {
+
+	}
+
+	public static void main(String[] args) {
+		
+		gato Gato = new Gato();
+		Livros livros = new Livros();
+		System.out.println(gato);
+		System.out.println(livros);/*System.out.println("Hello my friend");
+
+	*/
+	}
+}
+class Livros  {
+	private String nome;
+	private String npag;
+	
+}
